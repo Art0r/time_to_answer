04cb6aa47d21e02fadb0aff1ed4cdddf1d85bcb4
Arthur Fernandes <arthur0139@gmail.com>
2021-10-20 23:58:38 -0300

    Configurado o sb-admin para o admins backoffice

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "jquery"
+import "jquery.easing"
+import "popper.js@"
+import "chart.js"
 
 Rails.start()
 Turbolinks.start()
